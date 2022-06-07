3660bd9b5514cb41ab95a2bd7efcfc12d046e55b
skyhackvip <369983954@qq.com>
2022-06-07 16:42:27 +0800

    refactor

--- core/decision.go
+++
@@ -1,8 +0,0 @@
-package core
-
-type Decision struct {
-	Depends []string               `yaml:"depends,flow"`
-	Logic   string                 `yaml:"logic"`
-	Output  interface{}            `yaml:"output"` //该节点输出值
-	Assign  map[string]interface{} `yaml:"assign"` //赋值
-}
